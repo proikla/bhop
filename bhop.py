42903beacff59152549f8b6776734cb4ee17f592
proikla <menshikov1s12006@mail.ru>
2023-05-31 18:37:14 +0500

    dont use this

--- bhop.py
+++ bhop.py
@@ -1,22 +1,29 @@
 import keyboard
 import asyncio
 import pyautogui
-from time import sleep
+import mouse
+from time import sleep 
 k = keyboard
 
 paused = False
+
+# this shit wont work cuz csgo is stupid just like me 
 
 async def pause():
     while True:
         while k.is_pressed('='):
             paused = not paused
             sleep(0.1)
+        sleep(1)
 
 async def bhop():
     while True:
-        if keyboard.is_pressed('space'):
-            pyautogui.press('space')
-            sleep(0.01)
+        while keyboard.is_pressed('alt'):
+            mouse.wheel(-10)
+            sleep(20/100)
+        else:
+            print("ELSE")
+            sleep(.5)
 
 
 async def main():
