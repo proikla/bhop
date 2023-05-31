910cea6b7d27723964f4b3ffca7450a4be348efa
proikla <menshikov.here@gmail.com>
2023-05-31 11:29:42 +0500

    sdasdads

--- bhop.cpp
+++ bhop.cpp
@@ -1,34 +1,62 @@
 #include <Windows.h>
 #include <iostream>
-
+#include <thread>
 using namespace std;
-int main() 
+bool paused = false;
+
+void huy() 
 {
-    bool paused = false;
-    INPUT input;
-    input.type = INPUT_KEYBOARD;
-    input.ki.wScan = 0;
-    input.ki.time = 0;
-    input.ki.dwExtraInfo = 0;
-    input.ki.wVk = 0x20;
+    while(1)
+    {
+        cout << "is_paused: " << paused << "\r" << flush;
+        Sleep(200);
+    }
+}
+
+void stopOnVKEND() 
+{
     while(true)
     {
         if (GetAsyncKeyState(VK_END))
         {
             paused = !paused;
-            break;
-            Sleep(100);
+            Sleep(200);
         }
-        if (GetAsyncKeyState(VK_SPACE) < 0)
+    }
+    Sleep(200);
+}
+
+void bhopOnSpace()
+{
+    
+}
+
+int main() 
+{
+    
+    thread stopper(stopOnVKEND);
+    stopper.detach();
+    // thread bhop(bhopOnSpace);
+    // bhop.detach();
+    thread t1(huy);
+    t1.detach();
+    INPUT input;
+    input.type = INPUT_KEYBOARD;
+    input.ki.wScan = 0;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;                                                                                                     
+    input.ki.wVk = 0x20;
+
+    while(true)
+    {
+        if (GetKeyState(VK_SPACE) < 0 && !paused) 
         {
             input.ki.dwFlags = 0;
             SendInput(1, &input, sizeof(INPUT));
             input.ki.dwFlags = KEYEVENTF_KEYUP;
-            // Sleep(1);
             SendInput(1, &input, sizeof(INPUT));
+            Sleep(10);
         }
-        Sleep(10);
-        system("CLS");
     }
-
+    return 0;
 }
